b2f49bffe0416c56e11c9caf1f8e648c0dc162b8
Kea Mr <ptkien.code@gmail.com>
2021-11-30 18:01:51 +0700

    up

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -80,3 +80,13 @@ $(window).scroll(function() {
             event.preventDefault();
         });
     
+
+        document.querySelector('.demo-effect').onmousemove = (e) => {
+
+            const x = e.pageX - e.target.offsetLeft
+            const y = e.pageY - e.target.offsetTop
+          
+            e.target.style.setProperty('--x', `${ x }px`)
+            e.target.style.setProperty('--y', `${ y }px`)
+            
+          }
